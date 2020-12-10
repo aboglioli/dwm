1f4b1a7324fd19175003c63106014381dffb28b4
Alan Boglioli <alan.boglioli@gmail.com>
2020-12-09 21:48:34 -0300

    config: fix colors

--- config.def.h
+++ config.def.h
@@ -8,15 +8,15 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=9" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
+static const char col_gray2[]       = "#A5A58D";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char col_red[]        = "#20639B";
+static const char col_red[]         = "#E76F51";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_red,  col_red  },
+	[SchemeSel]  = { col_gray4, col_red,   col_red  },
 };
 
 /* tagging */
